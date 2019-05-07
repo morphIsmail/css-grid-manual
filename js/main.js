2307f6ce1e68d8de4353c178e23b0e8994be8dbf
morphIsmail <ismail_2016@mail.ru>
2019-05-07 17:21:51 +0300

    css grid manual

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href')
+    
+    document.querySelector('' + blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
